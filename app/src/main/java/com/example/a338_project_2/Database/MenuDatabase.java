9cf1cc72c83918017caf84c5284566e9920ca09b
juliussumagang <sumagang24julius@gmail.com>
2025-11-29 20:09:19 -0800

    setting up cart/database

--- app/src/main/java/com/example/a338_project_2/Database/MenuDatabase.java
+++ app/src/main/java/com/example/a338_project_2/Database/MenuDatabase.java
@@ -3,6 +3,7 @@ package com.example.a338_project_2.Database;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.MenuInflater;
 
 import androidx.annotation.NonNull;
 import androidx.room.Database;
@@ -10,13 +11,14 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import com.example.a338_project_2.Database.entities.Cart;
 import com.example.a338_project_2.Database.entities.User;
 import com.example.a338_project_2.MainActivity;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {User.class}, version = 1, exportSchema = false)
+@Database(entities = {User.class, Cart.class}, version = 2, exportSchema = false)
 public abstract class MenuDatabase extends RoomDatabase {
 
     private static final String DATABASE_NAME = "menu_database";
@@ -26,11 +28,11 @@ public abstract class MenuDatabase extends RoomDatabase {
 
     private static final int NUMBER_OF_THREADS = 4;
 
-    static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+    public static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
 
 
     @SuppressWarnings("deprecation")
-    static MenuDatabase getDatabase(final Context context){
+    public static MenuDatabase getDatabase(final Context context){
         if(INSTANCE == null){
             synchronized (MenuDatabase.class){
                 if(INSTANCE == null){
@@ -68,4 +70,6 @@ public abstract class MenuDatabase extends RoomDatabase {
 
     public abstract UserDAO userDAO();
     public abstract MenuDAO menuDAO();
+
+    public abstract CartDAO cartDAO();
 }
